d8796b0a8953ac2027c7d163601f41d6dbf8f4f9
Арина <78840926+ar1shka@users.noreply.github.com>
2021-05-24 20:07:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,75 +5,63 @@
 template<typename T>
 class TPQueue {
  private:
-    T* arr;
-    int size;
-    int begin,
-        end;
-    int count;
- public:
-TPQueue();
-    ~TPQueue();
-    void push(const T&);
-    T pop();
-    T get() const;
-    bool isEmpty() const;
-    bool isFull() const;
-};
-template<typename T>
-TPQueue<T>::TPQueue() : size(100),
-begin(0), end(0), count(0) {
-    arr = new T[size + 1];
-}
-template<typename T>
-TPQueue<T>::~TPQueue() {
-  delete [] arr;
-}
-template<typename T>
-void TPQueue<T>::push(const T & item) {
-  assert(count < size);
-  if (end != 0) {
-      for (int i = end - 1; i > -1; --i) {
-          if (arr[i].prior >= item.prior) {
-              arr[i + 1] = item;
-              continue;
-          }
-          if (arr[i].prior < item.prior) {
-              arr[i + 1] = arr[i];
-              if (i == 0)
-                  arr[i] = item;
-              continue;
-          }
-      }
-  } else {
-      arr[begin] = item;
+  T* arr;
+  int size;
+  int begin, end;
+  int count;
+  int stepBack(int index) {
+  int res = --index;
+  if (res < 0)
+  res += size + 1;
+  return res;
   }
-  count++;
-  end++;
+  int stepForward(int index) {
+  int res = ++index;
+  if (res > size)
+  res -= size + 1;
+  return res;
+  }
+
+ public:
+  TPQueue() :
+  size(100),
+  begin(0), end(0), count(0) {
+  arr = new T[size + 1];
+  }
+  ~TPQueue() {
+  delete[] arr;
+  }
+void push(const T & item) {
+assert(count < size);
+int cur = end;
+while (begin != cur && item.prior > arr[stepBack(cur)].prior) {
+arr[cur] = arr[stepBack(cur)];
+cur = stepBack(cur);
 }
-template<typename T>
-T TPQueue<T>::pop() {
-  assert(count > 0);
-  T item = arr[begin++];
-  count--;
-  if (begin > size)
-      begin -= size + 1;
-  return item;
+arr[cur] = item;
+end = stepForward(end);
+count++;
 }
-template<typename T>
-T TPQueue<T>::get() const {
-  assert(count > 0);
-  return arr[begin];
+T pop() {
+assert(count > 0);
+T item = arr[begin];
+count--;
+begin = stepForward(begin);
+return item;
 }
-template<typename T>
-bool TPQueue<T>::isEmpty() const {
-  return count == 0;
+T get() const {
+assert(count > 0);
+return arr[begin];
 }
-template<typename T>
-bool TPQueue<T>::isFull() const {
-  return count == size;
+bool isEmpty() const {
+return count == 0;
 }
+bool isFull() const {
+return count == size;
+}
+};
 struct SYM {
-  char ch;
-  int  prior;
+char ch;
+int  prior;
 };
 #endif // INCLUDE_TPQUEUE_H_
